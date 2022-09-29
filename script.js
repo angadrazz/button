8445d4c62dfb2fd599a41b2a532455477038a086
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-09-29 17:20:53 +0530

    using javascript in open new windows

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let newWindow;
 
 const openNewWindow = () => {
-  const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=300,height=300`;
+  const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=500,height=300`;
   newWindow = window.open('submit.html', 'submit', params);
 };
