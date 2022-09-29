46e8c1fcdf0204b50048793540308f4d0991a87b
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-09-29 17:15:44 +0530

    using script js

---
+++ script.js
@@ -0,0 +1,6 @@
+let newWindow;
+
+const openNewWindow = () => {
+  const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=300,height=300`;
+  newWindow = window.open('sub.html', 'sub', params);
+};
